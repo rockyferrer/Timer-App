6a52245d6f5fe70e7575a7f6d408b3d20bad613a
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-13 10:16:10 -0400

    initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">React Timer App</li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link" activeStyle={{
+                            fontWeight: 'bold'
+                        }}>Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/countdown" activeClassName="active" activeStyle={{
+                            fontWeight: 'bold'
+                        }}>Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by 
+                        <a href="www.github.com/rockyferrer" target="_blank">Rocky Ferrer</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    );
+
+};
+
+module.exports = Navigation;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Timer Component
+            </div>
+        );
+    }
+});
+
+module.exports = Timer;
